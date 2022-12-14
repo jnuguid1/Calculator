d857f72bbc334f82fb8841d2db5b8947b2f5bd2a
Jared <jarednuguid@gmail.com>
2022-12-14 12:10:52 -0500

    Style calculator and connect number functionality

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
+let display;
+const buttonRow = document.querySelectorAll('.button-row');
+const calculatorDisplay = document.querySelector('.calculator-display');
+
+buttonRow.forEach((element) => {
+    const buttons = element.children;
+    for (let i = 0; i < buttons.length; i++) {
+        const textValue = buttons[i].textContent;
+        buttons[i].addEventListener('click', () => {
+            display = textValue;
+            calculatorDisplay.textContent = textValue;
+        });
+    }
+});
+
 function add(a, b) {
     return a + b;
 };
