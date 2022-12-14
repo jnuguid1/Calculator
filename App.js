09d20c06dbae17199a00995520537f714849b613
Jared <jarednuguid@gmail.com>
2022-12-14 13:05:31 -0500

    Implement working calculator

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ let display;
 let operator;
 let operand1;
 let operand2;
+let calculation;
 const buttonRow = document.querySelectorAll('.button-row');
 const calculatorDisplay = document.querySelector('.calculator-display');
+const multiplyButton = document.querySelector('.multiply-button');
+const divideButton = document.querySelector('.divide-button');
+const addButton = document.querySelector('.plus-button');
+const minusButton = document.querySelector('.minus-button');
+const equalsButton = document.querySelector('.equals-button');
+const clearButton = document.querySelector('.clear-button');
 
 addButtonEventListener();
 
@@ -17,12 +24,45 @@ function addButtonEventListener() {
             }
             buttons[i].addEventListener('click', () => {
                 display = textValue;
-                console.log(display);
-                calculatorDisplay.textContent = textValue;
+                if (typeof operand1 == 'undefined') {
+                    operand1 = textValue;
+                    calculatorDisplay.textContent = textValue;
+                } else {
+                    operand2 = textValue;
+                    calculatorDisplay.textContent += " " + textValue;
+                }
             });
         }
     });
-}
+    multiplyButton.addEventListener('click', () => {
+        operator = multiply;
+        calculatorDisplay.textContent = operand1 + ' x'; 
+    });
+    divideButton.addEventListener('click', () => {
+        operator = division;
+        calculatorDisplay.textContent = operand1 + ' /';
+    });
+    addButton.addEventListener('click', () => {
+        operator = add;
+        calculatorDisplay.textContent = operand1 + ' +';
+    });
+    minusButton.addEventListener('click', () => {
+        operator = subtract;
+        calculatorDisplay.textContent = operand1 + ' -';
+    });
+    
+    equalsButton.addEventListener('click', () => {
+        calculation = operate(operator, operand1, operand2);
+        calculatorDisplay.textContent = calculation;
+    });
+
+    clearButton.addEventListener('click', () => {
+        operand1 = undefined;
+        operand2 = undefined;
+        operator = undefined;
+        calculatorDisplay.textContent = "";
+    });
+};
 
 function add(a, b) {
     return a + b;
